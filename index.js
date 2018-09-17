e6182966a33e1b5b6b329159f66b72ed9086eff1
henry vargas <hvargas@aventi.co>
2018-09-17 10:24:21 -0500

    cambio body

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const bodyParser = require('body-parser');
 const htmlToJson = require('html-to-json')
 const curl = require("curl");
 app.use(cors())
+
+// app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+app.use( (req, res, next)=> {
+    console.log(req.body)
+    console.log(req.method)
+    next();
+  }); 
+  
 app.get('/', (req, res) => { 
     res.send('Hello World!');
 });
@@ -14,6 +22,6 @@ require('./routes/mangaDetail')(app)
 require('./routes/capList')(app)
 require('./routes/mangaImages')(app)
 
-app.listen(3001, () => {
+app.listen(3012, () => {
     console.log('running');
 });
